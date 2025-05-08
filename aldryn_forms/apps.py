b1996ceb7f6009292d9d70a7acf3dbfc8b7c4c9f
Wolfgang Fehr <24782511+wfehr@users.noreply.github.com>
2025-05-08 09:27:07 +0200

    fixate "auto_field" on app-level
    - changing settings.DEFAULT_AUTO_FIELD would result in new migrations
    - if app-level sets the auto_field, which is used for the latest migration, no
      new migrations are created if the setting is changed

--- aldryn_forms/apps.py
+++ aldryn_forms/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AldrynForms(AppConfig):
     name = 'aldryn_forms'
     verbose_name = 'Aldryn forms'
+    default_auto_field = "django.db.models.BigAutoField"

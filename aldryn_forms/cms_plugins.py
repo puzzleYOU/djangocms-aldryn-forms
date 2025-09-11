eb8caa3038231bae940e6ae0cbdce699cf1f4c36
Florian Schieder <florian.schieder@puzzleyou.de>
2025-09-11 08:52:04 +0200

    CaptchaField: allow for being required

--- aldryn_forms/cms_plugins.py
+++ aldryn_forms/cms_plugins.py
@@ -1170,9 +1170,11 @@ else:
         form = CaptchaFieldForm
         form_field = CaptchaField
         form_field_widget = CaptchaTextInput
-        form_field_enabled_options = ['label', 'error_messages']
+        form_field_enabled_options = [
+            'label', 'required', 'error_messages'
+        ]
         fieldset_general_fields = [
-            'label',
+            'label', 'required'
         ]
         fieldset_advanced_fields = [
             'required_message',

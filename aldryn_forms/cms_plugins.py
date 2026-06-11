6d3c6fecea4db454d6668655f30c3544fc5c2cb4
Felix Holfelder <felix.holfelder@puzzleyou.de>
2026-06-11 07:04:41 +0200

    Revert "CaptchaField: allow for being required"
    This reverts commit 2d5f757ae9f608d3c84a61ca850a435fd2f660b8.

--- aldryn_forms/cms_plugins.py
+++ aldryn_forms/cms_plugins.py
@@ -1170,11 +1170,9 @@ else:
         form = CaptchaFieldForm
         form_field = CaptchaField
         form_field_widget = CaptchaTextInput
-        form_field_enabled_options = [
-            'label', 'required', 'error_messages'
-        ]
+        form_field_enabled_options = ['label', 'error_messages']
         fieldset_general_fields = [
-            'label', 'required'
+            'label',
         ]
         fieldset_advanced_fields = [
             'required_message',
